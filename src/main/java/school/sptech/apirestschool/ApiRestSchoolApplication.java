4397a2d8e81e92beb6e06420c41896856d061500
Bruno Henrique <bruno.lima@sptech.school>
2024-05-09 18:03:36 -0300

    feat: configuração inicial

---
+++ src/main/java/school/sptech/apirestschool/ApiRestSchoolApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.apirestschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSchoolApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO Teacher
+    (name)
+VALUES
+    ('Jonas'),
+    ('Josias'),
+    ('Luzia');
+
+INSERT INTO Student
+    (name, ra, teacher_id)
+VALUES
+    ('Bruno', '123', 1),
+    ('Paulo', '321', 1),
+    ('Caio', '456', 2),
+    ('João', '564', 3);
